5ad71a0129e2eb216d85a51488a358ff261027dc
aaron-leman0619 <83828785+aaron-leman0619@users.noreply.github.com>
2022-05-13 15:07:51 -0500

    Bug fix when adding process to warden pre initiate

--- lib/warden/assign.ts
+++ lib/warden/assign.ts
@@ -1,7 +1,8 @@
+import { logger } from "./../logging/logger";
 import { Process } from "./../processes/index";
 import Warden from "./index";
 
 export default function assign(this: Warden, process: Process) {
-  if (!this.processInterval) throw new Error("Warden not initiated.");
   this.processes[process.name] = process;
+  logger.debug(`Process ${process.name} assigned to Warden`);
 }
